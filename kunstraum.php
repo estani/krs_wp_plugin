7654335fb63535f3a6528f3ab25bbda721a9fc28
estani <estanislao.gonzalez@gmail.com>
2017-02-18 20:25:56 +0000

    first

---
+++ kunstraum.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: Kunst Raum Steglitz
+ * Plugin URI: http://danielpataki.com
+ * Description: This plugin adds special functions for Kunst Raum Steglitz
+ * Version: 1.0.0
+ * License: GPL2
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+define( 'KRS_PLUGIN', __FILE__ );
+define( 'KRS_PLUGIN_BASENAME', plugin_basename( KRS_PLUGIN ) );
+define( 'KRS_PLUGIN_NAME', trim( dirname( KRS_PLUGIN_BASENAME ), '/' ) );
+define( 'KRS_PLUGIN_DIR', untrailingslashit( dirname( KRS_PLUGIN ) ) );
+define( 'KRS_PLUGIN_URL', plugins_url('', KRS_PLUGIN) );
+define( 'KRS_PLUGIN_IMAGES_URL', KRS_PLUGIN_URL . '/images' );
+
+define( 'KRS_LOGO',  KRS_PLUGIN_IMAGES_URL . "/logo.jpg");
+define( 'KRS_PINK_COLOR', '#ec048c');
+define( 'KRS_A_LETTER_STYLE','font-style:italic;color:' . KRS_PINK_COLOR);
+
+require_once KRS_PLUGIN_DIR . '/menu/info.php';
+
+//[logo] - show KRS logo (default floating right)
+function logo_func( $atts ) {
+	$a = shortcode_atts( array(
+        	'position' => 'right'
+    		), $atts );
+	$style = "float:{$a['position']};";
+	return "<div><img style=\"$style\"; src=\"" . KRS_LOGO . "\"/></div>";
+}
+add_shortcode( 'logo', 'logo_func' );
+
+//wirtes
+function krs_func( $attrs ) {
+	return "<span>KUNST.R<span style=" . KRS_A_LETTER_STYLE . ">A</span>UM.STEGLITZ</span>";
+}
+add_shortcode( 'krs', 'krs_func');
