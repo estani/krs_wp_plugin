acf0d4b514b5589c96d6caef76df761a49931832
estani <estani@Blacky>
2017-02-19 13:46:07 +0100

    adding admin settings to change logo url

--- kunstraum.php
+++ kunstraum.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Plugin Name: Kunst Raum Steglitz
- * Plugin URI: http://danielpataki.com
  * Description: This plugin adds special functions for Kunst Raum Steglitz
  * Version: 1.0.0
  * License: GPL2
@@ -15,11 +14,10 @@ define( 'KRS_PLUGIN_DIR', untrailingslashit( dirname( KRS_PLUGIN ) ) );
 define( 'KRS_PLUGIN_URL', plugins_url('', KRS_PLUGIN) );
 define( 'KRS_PLUGIN_IMAGES_URL', KRS_PLUGIN_URL . '/images' );
 
-define( 'KRS_LOGO',  KRS_PLUGIN_IMAGES_URL . "/logo.jpg");
+define( 'KRS_LOGO',  KRS_PLUGIN_IMAGES_URL . "/logo.png");
 define( 'KRS_PINK_COLOR', '#ec048c');
 define( 'KRS_A_LETTER_STYLE','font-style:italic;color:' . KRS_PINK_COLOR);
 
-require_once KRS_PLUGIN_DIR . '/menu/info.php';
 
 //[logo] - show KRS logo (default floating right)
 function logo_func( $atts ) {
@@ -36,3 +34,7 @@ function krs_func( $attrs ) {
 	return "<span>KUNST.R<span style=" . KRS_A_LETTER_STYLE . ">A</span>UM.STEGLITZ</span>";
 }
 add_shortcode( 'krs', 'krs_func');
+
+
+require_once KRS_PLUGIN_DIR . '/menu/editor_info.php';
+require_once KRS_PLUGIN_DIR . '/menu/settings.php';
---
+++ menu/settings.php
@@ -0,0 +1,122 @@
+<?php // add the admin options page
+add_action('admin_menu', 'krs_plugin_admin_add_page');
+
+function krs_plugin_admin_add_page() {
+  add_options_page('KRS Settings', 'KRS Settings', 'manage_options', 'krs-settings', 'krs_plugin_options_page');
+}
+
+function krs_plugin_options_page() {
+?>
+<div>
+<h2>KRS plugin Settings</h2>
+Settings für den KRS Plugin
+<form action="options.php" method="post">
+<?php settings_fields('krs_plugin_options'); ?>
+<?php do_settings_sections('krs-settings'); ?>
+
+<input id="krs_save" disabled class="button-primary" name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+</form></div>
+
+<?php
+}
+
+add_action('admin_init', 'krs_plugin_admin_init');
+function krs_plugin_admin_init(){
+register_setting( 'krs_plugin_options', 'options'); //, 'krs_plugin_options_validate' );
+add_settings_section('krs_logo', 'Shortcode [logo]', 'krs_logo_section_text', 'krs-settings');
+add_settings_field('image_attachment_id', 'Logo Bild', 'plugin_setting_string', 'krs-settings', 'krs_logo');
+}
+/*
+function krs_plugin_options_validate() {
+  $newinput['url'] = trim($input['url']);
+  if(!preg_match('/^[a-z0-9]{32}$/i', $newinput['url'])) {
+    $newinput['url'] = '';
+  }
+  return $newinput;
+}
+*/
+
+function krs_logo_section_text() {
+  //Not required
+}
+function plugin_setting_string() {
+$options = get_option('options');
+$logo_id = $options['logo_id'];
+if ($logo_id == -1) {
+  $logo_url = KRS_LOGO;
+} else {
+  $logo_url = wp_get_attachment_url($logo_id);
+}
+
+wp_enqueue_media();
+//add_action( 'admin_enqueue_scripts', 'function_name' );
+
+?>
+<div class='image-preview-wrapper'>
+		<img id='image-preview' src='<?php echo $logo_url; ?>'  height='100' style='max-height: 100px;'>
+	</div>
+  <input type='hidden' name='options[logo_id]' id='image_attachment_id' value='<?php echo $logo_id; ?>'>
+	<input id="select_logo_button" style='width:180px;' type="button" class="button" value="<?php _e( 'Select Logo' ); ?>" />
+  <input id="use_default_logo_button" type="button" class="button" value="<?php _e( 'Use Default Logo' ); ?>" />
+  <?php
+}
+
+add_action( 'admin_footer', 'media_selector_print_scripts' );
+//add_action( 'admin_enqueue_scripts', 'media_selector_print_scripts' );
+function media_selector_print_scripts($hook) {
+
+  //echo serialize($hook);
+  $options = get_option('options');
+  $my_saved_attachment_post_id = $options['logo_id'];
+
+	?><script type='text/javascript'>
+
+		jQuery( document ).ready( function( $ ) {
+
+			// Uploading files
+			var file_frame,
+        previous_logo_id = <?php echo $my_saved_attachment_post_id; ?>,
+        selected_logo_id = previous_logo_id; // Set this
+
+      function newSelection(id, url) {
+        if (selected_logo_id === id) return;
+        $( '#image-preview' ).attr( 'src', url ).css( 'width', 'auto' );
+        $( '#image_attachment_id' ).val( id );
+        selected_logo_id = id;
+        $('#krs_save').prop('disabled', previous_logo_id === selected_logo_id);
+      }
+
+      $('#use_default_logo_button').on('click', function(){
+        newSelection(-1, '<?php echo  KRS_LOGO; ?>');
+      });
+
+			$('#select_logo_button').on('click', function(){
+				// If the media frame already exists, reopen it.
+				if ( !file_frame ) {
+  				// Create the media frame.
+  				file_frame = wp.media.frames.file_frame = wp.media({
+  					title: 'Select logo',
+  					button: {
+  						text: 'Use this image',
+  					},
+  					multiple: false	// Set to true to allow multiple files to be selected
+  				});
+
+  				// When an image is selected, run a callback.
+  				file_frame.on( 'select', function() {
+  					// We set multiple to false so only get one image from the uploader
+  					attachment = file_frame.state().get('selection').first().toJSON();
+  					// Do something with attachment.id and/or attachment.url here
+            newSelection(attachment.id, attachment.url)
+
+  				});
+        }
+				// Finally, open the modal
+				file_frame.open();
+			});
+
+		});
+
+	</script><?php
+
+}
